a948d22ccd8e8143b0d3e30257c3180ffdf7913a
Tom Penney <tpnn72@gmail.com>
2011-06-22 23:45:08 +0930

    Basic desctiption of various items.

---
+++ src/Items.java
@@ -0,0 +1,10 @@
+
+public class Items {
+	
+	public static String[] KeyDesc	= {"A rusty old key.","An old key.","It looks like it could open something...."};
+	public static String[] LockDesc	= {"An old old lock.","A scratched up lock.","A regular lock."};
+	public static String[] DeskDesc	= {"A fancy wooden desk.","A desk with a locked drawer.","It looks old."};
+	public static String[] WallDesc	= {"This wall is made of stone.","It looks old.","A cold stone wall."};
+	public static String[] BookDesc	= {"A worn out old book.","A torn up book.","It looks old."};
+
+}
