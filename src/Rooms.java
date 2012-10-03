c9d9cde583141a25784b310105e90f3c73af13b4
Tom Penney <tpnn72@gmail.com>
2012-10-03 15:07:11 +0930

    Added 'getDoors' method, removed slatherings of unnecessary methods and room maps, added javadoc comments
    New 'getDoors' method shows a list of all doors in the specified room.

--- src/Rooms.java
+++ src/Rooms.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /*
  * Rooms.
  * Class defines what the rooms look like with 2d arrays.
@@ -11,7 +13,7 @@
  * 4	Eastern Lounge
  * 5	Library
  * 6	Garden Room
- * 7	Balconey
+ * 7	Balcony
  * 8	Initiation Chamber
  * 9	Final Boss
  * 
@@ -33,23 +35,11 @@ public class Rooms {
 										{0,0,0,9,0,0,0},
 										{0,0,0,0,0,0,0}};
 	
-	public static int[][] GetRoom(int current) {
-		int[][] room	= {{}};
-		switch(current){
-		case 1:		room = StartingHall();		break;
-		case 2:		room = KingsHall();			break;
-		case 3:		room = TLobby();			break;
-		case 4:		room = EasternLounge();		break;
-		case 5:		room = Library();			break;
-		case 6:		room = GardenRoom();		break;
-		case 7:		room = Balconey();			break;
-		case 8:		room = InitiationChamber();	break;
-		case 9:		room = FinalBoss();			break;
-		}
-		
-		return room;
-	}
-	
+	/**
+	 * Tests if the specified room's door is locked or not.
+	 * @param roomId	the id of the room to test
+	 * @return	true if the door is locked
+	 */
 	public static boolean isLocked(int roomId) {
 		if(keyRequired[roomId-1] != 0) {
 			return true;
@@ -58,18 +48,54 @@ public class Rooms {
 		}
 	}
 	
+	/**
+	 * Unlocks the door of the specified room.
+	 * @param roomId	the id of the room to unlock
+	 */
 	public static void unlockDoor(int roomId) {
 		keyRequired[roomId-1]	= 0;
 	}
 	
+	/**
+	 * Finds the id of the key required to unlock the specified room.
+	 * @param roomId	the id of the room to find the key id for
+	 * @return	the key id
+	 */
 	public static int keyIdRequired(int roomId) {
 		return keyRequired[roomId-1];
 	}
 	
+	/**
+	 * Returns an ArrayList of the cardinal direction of the rooms connected to the specified room id.
+	 * @param roomId	the room id to find the doors of
+	 * @return	list of doors available
+	 */
+	public static ArrayList<String> getDoors(int roomId) {
+		ArrayList<String> doors	= new ArrayList<String>();
+		
+		if(getNextRoomId("north", roomId) != 0) {
+			doors.add("North");
+		} if(getNextRoomId("east", roomId) != 0) {
+			doors.add("East");
+		} if(getNextRoomId("south", roomId) != 0) {
+			doors.add("South");
+		} if(getNextRoomId("west", roomId) != 0) {
+			doors.add("West");
+		}
+		
+		return doors;
+	}
+	
+	/**
+	 * Finds the id of the next room in the specified direction.
+	 * @param direction		the cardinal direction to test
+	 * @param thisRoomId	the id of the room to test
+	 * @return	the id of the next room or 0 if no room is found in the specified direction
+	 */
 	public static int getNextRoomId(String direction, int thisRoomId) {
 		// Get currentRoom coords
-		int y	= -1;
-		int x	= -1;
+		int y	= 0;
+		int x	= 0;
 		boolean foundCoords	= false;
 		for(int yi=0; yi<layout.length; yi++) {
 			for(int xi=0; xi<layout[0].length; xi++) {
@@ -106,7 +132,13 @@ public class Rooms {
 		return thisRoomId;
 	}
 	
-	public static int[][] ChangeRoom(String direction, int CurrentRoom, boolean start) {
+	/**
+	 * Changes the current room to the room in the specified cardinal direction
+	 * @param direction		cardinal direction to change rooms in
+	 * @param CurrentRoom	the id of the current room
+	 * @param start		whether or not the game is starting in this room
+	 */
+	public static void ChangeRoom(String direction, int CurrentRoom, boolean start) {
 		boolean broken	= false;
 		int NewRoom		= 0;
 		
@@ -163,12 +195,6 @@ public class Rooms {
 			NewRoom	= 1337;
 		}
 		
-		int[][] room;
-		if(start) {
-			room	= GetRoom(1);
-		} else {
-			room	= GetRoom(NewRoom);
-		}
 		for(int i=0; i<visitedRooms.length; i++) {
 			if(visitedRooms[i] == 0) {
 				visitedRooms[i]	= currentRoom;
@@ -176,168 +202,114 @@ public class Rooms {
 			}
 		}
 		RoomChange		= true;
-		return room;
 	}
-
-	public static int[][] StartingHall() {
-		int[][] room	=  {{1,1,1,1,1,1,1},
-							{1,0,8,10,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,1,1,1,1,1,1}};
+	
+	/**
+	 * Room information for the Starting Hall
+	 */
+	public static void StartingHall() {
 		
 		currentRoomName	= "Starting Hall";
 		currentRoom		= 1;
 		RoomDescription	= "The room is small, square and made of solid stone. It is cold and there is a ladder " +
 				"going up through a hatch.";
-		return room;
+		
 	}
 	
-	public static int[][] KingsHall() {
-		
-		// Room number	= 2;
-		int[][] room	=  {{1,1,1,1,1,1,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,1,1,1,1,1,1}};
+	/**
+	 * Room information for the King's Hall
+	 */
+	public static void KingsHall() {
 		currentRoomName	= "King's Hall";
 		currentRoom		= 2;
 		RoomDescription	= "You climb through the hatch and you are now in a bigger hall. There are red carpets magestically " +
 				"hanging from the walls, next to the enormous painting of a proud looking king. There is one door at the " +
 				"South of the room.";
-		return room;
+		
 	}
 	
-	public static int[][] TLobby() {
-		
-		// Room number	= 3;
-		int[][] room	=  {{1,1,1,1,1,1,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,1,1,1,1,1,1}};
+	/**
+	 * Room information for the T-Lobby
+	 */
+	public static void TLobby() {
 		currentRoomName	= "T-Lobby";
 		currentRoom		= 3;
 		RoomDescription	= "As you close the wooden door behind you, you are rushed with a sense of fear from the sound of a " +
 				"shotgun and the intense wind coming from the eastern doorway.";
-		return room;
+		
 	}
 	
-	public static int[][] EasternLounge() {
-		
-		// Room number	= 4;
-		int[][] room	=  {{1,1,1,1,1,1,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,1,1,1,1,1,1}};
+	/**
+	 * Room information for the Eastern Lounge
+	 */
+	public static void EasternLounge() {
 		currentRoomName	= "Eastern Lounge";
 		currentRoom		= 4;
 		RoomDescription	= "You walk into the room and you catch a glimps of a man wearing a cowboy hat and dark clothing as he " +
 				"runs out of the room through the huge eastern doors. There is a slight smell of cheap aftershave and you can " +
 				"see the sun begining to set through the window.";
-		return room;
+		
 	}
 	
-	
-	public static int[][] Library() {
-		
-		// Room number	= 5;
-		int[][] room	=  {{1,1,1,1,1,1,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,1,1,1,1,1,1}};
+	/**
+	 * Room information for the Library
+	 */
+	public static void Library() {
 		currentRoomName	= "Library";
 		currentRoom		= 5;
 		RoomDescription	= "After taking one step into the badly lit room, the big wooden doors slam behind you from a gust of " +
 				"warm wind. There is no light switch and the room has a stench of old books. There is a door at the end of the " +
 				"room that looks like it was just used as you briefly see the last sketch of light from the closing door.";
-		return room;
+		
 	}
 	
-	public static int[][] GardenRoom() {
-		
-		// Room number	= 6;
-		int[][] room	=  {{1,1,1,1,1,1,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,1,1,1,1,1,1}};
+	/**
+	 * Room information for the Garden Room
+	 */
+	public static void GardenRoom() {
 		currentRoomName	= "Garden Room";
 		currentRoom		= 6;
 		RoomDescription	= "You nostrils are assulted with the pungant smell of roses and various other flowers. The sudden " +
 				"change of light makes you double back slightly as you slowly try to gain back your vision. This is clearly" +
 				"taken care of very often as every plant is perfecly shaped into the cliche form of itself.\n\n" +
 				"There is but one gold wilted lilly laying in the centre of the greenhouse auraed room and a door at the end.";
-		return room;
+		
 	}
 	
-	public static int[][] Balconey() {
-		
-		// Room number	= 7;
-		int[][] room	=  {{1,1,1,1,1,1,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,1,1,1,1,1,1}};
+	/**
+	 * Room information for the Balcony
+	 */
+	public static void Balconey() {
 		currentRoomName	= "Balconey";
 		currentRoom		= 7;
 		RoomDescription	= "The wafer thin doors are almost crushed in your hands as you open them and enter the balconey." +
 				"You are immediately faced with the dark figure you saw earlier; only this time, he is slung over the " +
 				"balconey with a knife in his hand and blood pouring out from his neck. You are at a dead end.";
-		return room;
+		
 	}
 	
-	public static int[][] InitiationChamber() {
-		
-		// Room number	= 8;
-		int[][] room	=  {{1,1,1,1,1,1,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,1,1,1,1,1,1}};
+	/**
+	 * Room information for the Initiation Chamber
+	 */
+	public static void InitiationChamber() {
 		currentRoomName	= "Initiation Chamber";
 		currentRoom		= 8;
 		RoomDescription	= "The steel doors are heavy so you charge into it and they burst open, slamming in to the wall it is " +
 				"hinged to. The room is cold and bright. There is a velvet rope across the front of the door ahead of you. " +
 				"In the room there are three desks, two on the left and one to your right. All of them have a drawer with a " +
 				"lock on it.";
-		return room;
+		
 	}
 	
-	public static int[][] FinalBoss() {
-		
-		// Room number	= 9;
-		int[][] room	=  {{1,1,1,1,1,1,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,0,0,0,0,0,1},
-							{1,1,1,1,1,1,1}};
+	/**
+	 * Room information for the Final Boss
+	 */
+	public static void FinalBoss() {
 		currentRoomName	= "Final Boss";
 		currentRoom		= 9;
 		RoomDescription	= "The door swings open and as you take a step in, the lights turn on and confetti falls from the " +
 				"ceiling, a party of giant dancing lobsters come out from hiding and start to boogie.\n\n" +
 				"Congratulations, you have just won Corlanthia";
-		return room;
+		
 	}
 }
